10812869d8012f251a2af3109ca64dadd3b5d6ca
Vasco Marques <2232347@my.ipleiria.pt>
2024-11-14 20:39:47 +0000

    Alteração RBAC e View Index Backend

--- TicketGo/backend/views/site/index.php
+++ TicketGo/backend/views/site/index.php
@@ -1,29 +1,15 @@
 <?php
-use yii\helpers\Html;
-use yii\helpers\Url;
-
 $this->title = 'Dashboard';
+$this->params['breadcrumbs'] = [['label' => $this->title]];
 ?>
-
-<div class="site-index">
-    <div class="jumbotron">
-        <h1><?= Html::encode($this->title) ?></h1>
-    </div>
+<div class="container-fluid">
 
     <div class="row">
-        <div class="col-lg-4">
-            <div class="small-box">
-                <div class="inner">
-                    <h3>Eventos</h3>
-                    <p>Numero de Eventos</p>
-                </div>
-                <div class="icon">
-                    <i class="fas fa-calendar-alt"></i>
-                </div>
-                <a href="<?= Url::to(['evento/index']) ?>" class="small-box-footer">
-                    Ver Eventos <i class="fas fa-arrow-circle-right"></i>
-                </a>
-            </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+            <?= \hail812\adminlte\widgets\SmallBox::widget([
+                'title' => 'Eventos',
+                'text' => 'Numero de eventos',
+            ]) ?>
         </div>
-
+    </div>
 </div>
